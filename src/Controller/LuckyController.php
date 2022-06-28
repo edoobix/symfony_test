4b360a327901dc99b0936a10b08b7a11618b4922
edoobix <edoobix@gmail.com>
2022-06-28 15:13:23 +0400

    test_v1.01

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -8,13 +8,21 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyController extends AbstractController
 {
-    # @Route("/lucky/number")
-    public function number(): Response
-    {
-        $number = random_int(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+    #[Route("/lucky/number/{name?}")]
+    public function number($name): Response
+    {
+        $number = random_int(0, 2);
+
+
+        if($number == 1 && $name != null){
+            return new Response(
+                "<html><body>You are WINNER, {$name}</body></html>"
+            );
+        } else {
+            return new Response(
+                '<html><body>Lucky number: '.$number.'</body></html>'
+            );    
+        }
     }
 }
